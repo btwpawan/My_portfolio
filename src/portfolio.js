09a879aab377bd6a4fe09ade4120138fb3e0b300
btwpawan <pawansanap04@gmail.com>
2025-07-26 17:52:06 +0530

    Customized developerFolio portfolio

---
+++ src/portfolio.js
@@ -0,0 +1,310 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+
+// Splash Screen
+
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 1500 // Set animation duration as per your animation
+};
+
+// Summary And Greeting Section
+
+const illustration = {
+  animated: true // Set to false to use static SVG
+};
+
+const greeting = {
+  username: "Pawan Sanap",
+  title: "Hi all, I'm Pawan",
+  subTitle: emoji(
+    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+github: "https://github.com/btwpawan",
+linkedin: "https://www.linkedin.com/in/pawansanap",
+gmail: "pawansanap04@gmail.com",
+
+  // Instagram, Twitter and Kaggle are also supported in the links!
+  // To customize icons and social links, tweak src/components/SocialMedia
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+        "⚡ Develop fully responsive and interactive Frontend UIs using React.js and modern CSS libraries like Tailwind & Bootstrap.",
+
+    ),
+    emoji("⚡Build RESTful backend APIs using Java, Spring Boot and integrate them with MySQL for full stack web applications."),
+    emoji(
+        "⚡ Deploy and manage full stack projects on platforms like Vercel, Render, and GitHub Pages; familiar with Firebase basics and Git for version control.",
+  )
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "Java",
+      fontAwesomeClassname: "fab fa-java"
+    },
+    
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+  skillName: "springboot",
+  imageSrc: require("./assets/images/springboot.png"), // Place the logo image here
+      
+    },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "fab fa-git-alt"
+    },
+    {
+      skillName: "github",
+      fontAwesomeClassname: "fab fa-github"
+    },
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+const Projects = {
+  title: "Projects",
+  projects: [
+    {
+      image: require("./assets/images/tesla.jpg"),
+      projectName: "Tesla Clone (Electric Car Website UI)",
+      projectDesc: "A modern, fully responsive clone of the Tesla homepage built using React and styled-components. Includes smooth animations, scroll effects, and UI sections showcasing Tesla's vehicle lineup and clean energy products.",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://tesla-clone-drab-six.vercel.app/"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/netflix.jpg"),
+      projectName: "Netflix Clone (Streaming UI)",
+      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://netflix-clone-ten-peach-11.vercel.app/"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/pass.jpg"),
+      projectName: "React Password Generator",
+      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://react-password-generator-khaki.vercel.app/"
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "Savitribai Phule Pune University",
+      logo: require("./assets/images/harvardLogo.png"),
+      subHeader: "Bachelor of Computer Applications (BCA) | CGPA 8.27 (A+ Grade)",
+      duration: "September 2022 - April 2025",
+      descBullets: [
+                  "Completed add-on certifications in Full Stack Java Development",
+                  "Built major project: Car Rental System using Spring Boot & MySQL",
+                  " Participated in inter-college coding competitions",
+                  "Active member of college coding club; organized coding workshops and participated in hackathons"
+                 ]
+    },
+    {
+      schoolName: "Balwantrao Kadam high School,Yelambghat",
+      logo: require("./assets/images/stanfordLogo.png"),
+      subHeader: "HSC Science | Percentage: 86% – Maharashtra State Board",
+      duration: "September 2021 - April 2022",
+      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+      descBullets: ["Learned core concepts of algorithms, basic Java, and networking"]
+    },
+    {
+      schoolName: "D.B Ghumare School , Beed",
+      logo: require("./assets/images/stanfordLogo.png"),
+      subHeader: "SSC | Percentage: 84% – Maharashtra State Board",
+      duration: "September 2019 - April 2020",
+      desc: "Passed with distinction and developing a strong foundation in Mathematics and Computers.",
+      descBullets: ["Secured A+ in Maths and Science"]
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: false, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "90%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "70%"
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "60%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+const openSource = {
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN, // Set your GitHub token in .env file
+  githubUserName: "btwpawan", // Your GitHub username to fetch the data
+  showGithubProfile: "true", // Set false to hide this section, defaults to true
+  display: true // Set false to hide this section, defaults to true
+};
+// Some big projects you have worked on
+
+
+// Achievement Sectionf
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆"),
+  subtitle:
+    "Certifications Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "Full Stack Java Developer – Teknowell Edutech",
+      subtitle:
+        "Completed hands-on training in Core Java, Advanced Java, JDBC, JSP, Servlets, Spring, Spring Boot, and React with real-time project development.",
+      image: require("./assets/images/Teknowell logo.webp"),
+      imageAlt: "Google Code-In Logo",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
+        },
+    ]
+    },
+    {
+      title: "Java Programming Masterclass - Udemy ",
+      subtitle:
+        "Completed 80+ hours of Java training covering OOPs, Exception Handling, Collections, Multithreading, and File Handling.",
+      image: require("./assets/images/Udemy.png"),
+      imageAlt: "Google Assistant Action Logo",
+      footerLink: [
+        {
+          name: "View Google Assistant Action",
+          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+        }
+      ]
+    },
+
+    {
+      title: "DSA with Java – Apna College",
+      subtitle: "Completed structured DSA course with in-depth focus on Arrays, Strings, Recursion, Sorting, Binary Search, LinkedList, Trees, Graphs, and Dynamic Programming.",
+      image: require("./assets/images/apnacollege.png"),
+      imageAlt: "PWA Logo",
+      footerLink: [
+        {name: "Certification", url: ""},
+        {
+          name: "Final Project",
+          url: "https://pakistan-olx-1.firebaseapp.com/"
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Resume Section
+const resumeSection = {
+  title: "Resume",
+  subtitle: "Feel free to download my resume",
+
+  // Please Provide with Your Podcast embeded Link
+  display: false // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "+91-8788493504",
+  email_address: "pawansanap04@gmail.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "twitter", //Replace "twitter" with your twitter username without @
+  display: true // Set true to display this section, defaults to false
+};
+
+const isHireable = true; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  splashScreen,
+  skillsSection,
+  educationInfo,
+  techStack,
+  openSource,
+  Projects,
+  achievementSection,
+  contactInfo,
+  twitterDetails,
+  isHireable,
+  resumeSection
+};
